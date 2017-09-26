45334211d85e343d7f3c1aa128a22da3acaa71e7
Jacob Luetzow <jacob@devslopes.com>
2017-09-26 00:49:07 -0700

    removed v1

--- src/index.js
+++ src/index.js
@@ -23,20 +23,20 @@ app.use(bodyParser.json({
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    // api routes v1 (https://api.devslopes.com/tikiBar/v1)
-    app.use('/v1', routes);
+    // api routes v1 (https://api.devslopes.com/tikibar)
+    app.use('/tikibar', routes);
     break;
   case 'staging':
     // api routes /staging/v1
-    app.use('/staging/v1', routes);
+    app.use('/staging', routes);
     break;
   case 'development':
     // api routes /dev/v1
-    app.use('/dev/v1', routes);
+    app.use('/dev', routes);
     break;
   default:
     // api routes /localdev/v1
-    app.use('/localdev/v1', routes);
+    app.use('/localdev', routes);
     break;
 }
 
